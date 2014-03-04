ab309474eb78cad98aee27e084027b665d048eed
Mark Madsen <mark@agileanimal.com>
2014-03-04 09:59:29 -0700

    move back to / for compatibility

--- lib/grape-appsignal/middleware.rb
+++ lib/grape-appsignal/middleware.rb
@@ -14,7 +14,7 @@ module Appsignal
         request_path = env['api.endpoint'].routes.first.route_path[1..-1].gsub(/\(\.:format\)\z/, "")
 
         metric_name  = "process_action.grape#{metric_path}"
-        action = "#{method}#{metric_path}"
+        action = "#{method}#{env['PATH_INFO']}"
 
         ActiveSupport::Notifications.instrument(metric_name, { method: method, path: request_path, action: action, class: "API" } ) do |payload|
           @app.call(env)
